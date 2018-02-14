35ba1df4a361c75adcaf2e3905fd6d9aed4c38c6
Janjanrey <janrey@github.com>
2018-02-14 18:36:03 +0800

    dynamic names

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -282,8 +282,8 @@
                     <img src="{{ asset('assets/images/user1.png') }}" width="50" height="50" alt="User" />
                 </div>
                 <div class="info-container">
-                    <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">John Doe</div>
-                    <div class="email">john.doe@example.com</div>
+                    <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</div>
+                    <div class="email">{{ Auth::user()->username }}</div>
                     <div class="btn-group user-helper-dropdown">
                         <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
                         <ul class="dropdown-menu pull-right">
